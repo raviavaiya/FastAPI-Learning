c827faaaf8c5cab136e88597d1b15ce1895718d5
raviavaiya <raviavaiya03@gmail.com>
2025-03-27 17:25:40 +0530

    First APi

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello FastAPI"}
 
+@app.get("/items/{item_id}")
+async def read_item(item_id:int):
+    return {"item_id": item_id,"message":"Item fetched successfully"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# run the code with Windows
+# python -m uvicorn main:app --reload
